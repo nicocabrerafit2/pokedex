3214926c9168bc7bfa32bda69e9dd99d69bfcf6f
Nicolas D. Cabrera PC escritorio <nicocabrera8@outlook.com>
2024-05-31 11:27:18 -0300

    se crean rutas a vistas

--- src/app.js
+++ src/app.js
@@ -1,6 +1,12 @@
 const express = require("express");
-
+const path = require("path");
+//const router = require("express-router")();
 const app = express();
+
+app.get("/", (req, res) => {
+  const routeToAboutHTML = path.join(__dirname, "./views/about.html");
+  res.sendFile(routeToAboutHTML);
+});
 
 const puerto = 3001;
 app.listen(puerto, () => {
