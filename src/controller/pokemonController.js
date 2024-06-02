47163a123efc72e7c96a7f8ccb766be08bf8146c
Nicolas D. Cabrera PC escritorio <nicocabrera8@outlook.com>
2024-06-01 22:55:25 -0300

    realizo pequeña logica para utilizar los query params

--- src/controller/pokemonController.js
+++ src/controller/pokemonController.js
@@ -1,7 +1,19 @@
 const path = require("path");
 const controller = {
   pokemonGet: (req, res) => {
-    res.sendFile(path.join(__dirname, "../views/pokemon.html"));
+    const pokemonesList = [
+      { nombre: "picachu", id: 1 },
+      { nombre: "alakazam", id: 2 },
+    ];
+
+    resultPokemon = pokemonesList.find((pokemon) => {
+      return pokemon.id === Number(req.params.id);
+    });
+    console.log(resultPokemon);
+    if (resultPokemon !== undefined) {
+      return res.sendFile(path.join(__dirname, "../views/pokemon.html"));
+    }
+    return res.send("No se encontro ese pokemon");
   },
 };
 
