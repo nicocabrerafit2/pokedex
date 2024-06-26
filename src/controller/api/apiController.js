e2d0d7fe7900dcf88beb7e42ca2c2642fec61dfb
Nicolas D. Cabrera PC escritorio <nicocabrera8@outlook.com>
2024-06-26 20:32:41 -0300

    agrego en el home un selector por tipos

--- src/controller/api/apiController.js
+++ src/controller/api/apiController.js
@@ -4,39 +4,32 @@ const controller = {
   home: (req, res) => {
     res.render("home");
   },
-  getPokemon: (req, res) => {
-    /*fetch("https://json.freeastrologyapi.com/", {
-      method: "GET",
-      headers: {
-        "Content-Type": "application/json",
-        "x-api-key": "6gRIA9kmx49wQievzg4sF5KmcyLT0LEF8E73N2cQ",
-      },
-    })
-      .then((response) => response.json())
-      .then((data) => console.log(data))
-      .catch((error) => console.error("Error:", error));
+  getPokemon: async (req, res) => {
+    try {
+      const search = req.query.name;
+      const url = `https://pokeapi.co/api/v2/pokemon/${search}`;
 
-    */
-
-    const search = req.query.name;
-
-    fetch("https://pokeapi.co/api/v2/pokemon/" + search)
-      .then((response) => response.json())
-      .then((data) => {
-        const dataPokemon = {
-          Pokemon: data.name,
-          Peso: data.weight / 10,
-          Altura: data.height * 10,
-          Tipo: data.types[0].type.name,
-          Imagen: data.sprites.front_default,
-        };
-
-        return res.render("pokemon", { dataPokemon });
-      })
-      .catch((error) => {
-        console.error("Error:", error);
-        res.status(500).send(error);
+      const result = await fetch(url, {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          "x-api-key": "6gRIA9kmx49wQievzg4sF5KmcyLT0LEF8E73N2cQ",
+        },
       });
+      const pokemon = await result.json();
+
+      const dataPokemon = {
+        Pokemon: pokemon.name,
+        Peso: pokemon.weight / 10,
+        Altura: pokemon.height * 10,
+        Tipo: pokemon.types[0].type.name,
+        Imagen: pokemon.sprites.front_default,
+      };
+      return res.render("pokemon", { dataPokemon });
+    } catch (error) {
+      console.error("Se produjo el siguiente error:", error);
+      res.status(500).send(error);
+    }
   },
 };
 
